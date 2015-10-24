5b8e63b25961106dc6d76c739617c31da19c5c44
fanfank <reetsee@qq.com>
2015-10-25 01:20:39 +0800

    Finish writing proxy demo. but not tested

--- demo/proxy_demo/proxy.c
+++ demo/proxy_demo/proxy.c
@@ -1,3 +1,4 @@
+//NOTE: this demo does not gurantee that there is no memory leak
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,9 +9,13 @@
 
 #include "uv.h"
 
+#include "rts_buf.c"
+
 #define DEFAULT_PORT 8888
 #define DEFAULT_BACKLOG 128
 #define DEFAULT_NWORKERS 5
+#define UPSTREAM_HOST "127.0.0.1"
+#define UPSTREAM_PORT "4444"
 
 pid_t parent_pid;
 pid_t cur_pid;
@@ -19,10 +24,28 @@ pid_t g_pids[DEFAULT_NWORKERS];
 uv_loop_t* loop;
 
 int spawn_processes(int nworkers);
-void on_new_connection(uv_stream_t* server, int status);
+void on_new_downstream_connection(uv_stream_t* server, int status);
 void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
-void echo_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf);
-void echo_write(uv_write_t* req, int status);
+void downstream_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf);
+void upstream_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf);
+void after_upstream_write(uv_write_t* req, int status);
+int init_upstream_connection(uv_stream_t* downstream);
+void on_upstream_connect(uv_connect_t* connect, int status);
+int init_callback_data(void* data);
+int cleanup_callback_data(uv_stream_t* stream);
+int transfer_stream_data(uv_stream_t* from, uv_stream_t* to, uv_write_cb wrcb);
+
+typedef struct stream_conn_s stream_conn_t;
+struct stream_conn_s {
+    uv_stream_t* stream;
+    unsigned ready: 1;
+};
+
+typedef struct callback_data_s callback_data_t;
+struct callback_data_t {
+    stream_conn_t* stream_conn;
+    rts_buf_t* buf;
+};
 
 int main() {
     parent_pid = getpid();
@@ -38,7 +61,7 @@ int main() {
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
 
     int32_t r = uv_listen((uv_stream_t*)&server, DEFAULT_BACKLOG, 
-            on_new_connection);
+            on_new_downstream_connection);
     if (r) {
         fprintf(stderr, "Listen error %s\n", uv_strerror(r));
         return 1;
@@ -60,7 +83,6 @@ int main() {
         }
         return 0;
     } else {
-        //TODO 
         return uv_run(loop, UV_RUN_DEFAULT);
     }
 }
@@ -91,7 +113,18 @@ int spawn_processes(int nworkers) {
     return 0;
 }
 
-void on_new_connection(uv_stream_t* server, int status) {
+int init_callback_data(callback_data_t* data) {
+    data = (callback_data_t*)malloc(sizeof(callback_data_t));
+    if (data == NULL) {
+        return -1;
+    }
+
+    data->stram_conn = NULL;
+    data->buf = NULL;
+    return 0;
+}
+
+void on_new_downstream_connection(uv_stream_t* server, int status) {
     fprintf(stdout, "child [%u] handling new connection...\n", cur_pid);
     
     if (status < 0) {
@@ -100,10 +133,12 @@ void on_new_connection(uv_stream_t* server, int status) {
     }
 
     uv_tcp_t* client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    init_callback_data((callback_data_t*)client->data);
     uv_tcp_init(loop, client);
     if (uv_accept(server, (uv_stream_t*)client) == 0) {
-        uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+        uv_read_start((uv_stream_t*)client, alloc_buffer, downstream_read);
     } else {
+        cleanup_callback_data((uv_stream_t*)client);
         uv_close((uv_handle_t*)client, NULL);
     }
 }
@@ -114,21 +149,47 @@ void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
     buf->len  = suggested_size;
 }
 
-void echo_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
-    fprintf(stderr, "echo_read called\n");
+void downstream_read(uv_stream_t* client, ssize_t nread, 
+        const uv_buf_t* buf) {
+
+    fprintf(stderr, "downstream_read called\n");
     if (nread < 0) {
         if (nread != UV_EOF) {
-            fprintf(stderr, "Read error [%s] in process [%u]\n",
+            fprintf(stderr, "downstream_read error [%s] in process [%u]\n",
                     uv_err_name(nread), cur_pid);
         }
+
+        if (client->data->stream_conn) {
+            cleanup_callback_data(client->data->stream_conn->stream);
+            uv_close((uv_handle_t*)client->data->stream_conn->stream, NULL);
+        }
+
+        cleanup_callback_data(client);
         uv_close((uv_handle_t*)client, NULL);
     } else if (nread > 0) {
-        uv_write_t* req = (uv_write_t*)malloc(sizeof(uv_write_t));
-        uv_buf_t wrbuf  = uv_buf_init(buf->base, nread);
-        uv_write(req, client, &wrbuf, 1, echo_write);
         buf->base[nread] = '\0';
-        //fprintf(stdout, "server received %s\n", buf->base);
         fprintf(stdout, "%s", buf->base);
+
+        callback_data_t cb_data = (callback_data_t*)client->data;
+        if (cb_data->stream_conn == NULL) {
+            if (init_upstream_connection(client) != 0) {
+                fprintf(stderr, "inti upstream connection error\n");
+                cleanup_callback_data(client);
+                uv_close((uv_handle_t*)client, NULL);
+                if (buf->base) {
+                    free(buf->base);
+                }
+                return;
+            }
+        }
+
+        //简化设计，将内容先全部读到下游数据缓存中
+        rts_buf_append(cb_data, buf->base, nread);
+
+        if (cb_data->stream_conn->ready) {
+            transfer_stream_data(client, client->data->stream_conn, 
+                    after_upstream_write);
+        }
     }
 
     if (buf->base) {
@@ -136,11 +197,156 @@ void echo_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
     }
 }
 
-void echo_write(uv_write_t* req, int status) {
+void after_upstream_write(uv_write_t* req, int status) {
     if (status) {
-        fprintf(stderr, "Write error [%s] in process [%u]\n",
+        fprintf(stderr, "write to upstream error [%s] in process [%u]\n",
                 uv_strerror(status), cur_pid);
     }
     fprintf(stderr, "echo_write called\n");
     free(req);
+}
+
+int init_upstream_connection(uv_stream_t* downstream) {
+    if (downstream == NULL || downstream->data != NULL) {
+        return -1;
+    }
+
+    struct sockaddr_in dest;
+    uv_ip4_addr(UPSTREAM_HOST, UPSTREAM_PORT, &dest);
+
+    uv_tcp_t* upstream = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    uv_connect_t* conn = (uv_connect_t*)malloc(sizeof(uv_connect_t));
+    stream_conn_t* downstream_conn = 
+            (stream_conn_t*)malloc(stream_conn_t);
+    stream_conn_t* upstream_conn = 
+            (stream_conn_t*)malloc(stream_conn_t);
+
+    int init_cb_data_res = init_callback_data((callback_data_t*)upstream->data);
+
+    if (!upstream || !conn || !downstream_conn || !upstream_conn 
+            || init_cb_data_res != 0) {
+        free(upstream);
+        free(conn);
+        free(downstream_conn);
+        free(upstream_conn);
+        return -2;
+    }
+
+    downstream_conn->stream = downstream;
+    downstream_conn->ready = 1;
+
+    upstream_conn->stream = upstream;
+    upstream_conn->ready = 0;
+
+    downstream->data->stream_conn = upstream_conn;
+    upstream->data->stream_conn   = downstream_conn;
+    
+    uv_tcp_init(loop, upstream);
+    uv_tcp_connect(conn, upstream, (struct sockaddr*)&dest,
+            on_upstream_connect);
+
+    return 0;
+}
+
+void on_upstream_connect(uv_connect_t* connect, int status) {
+
+    stream_conn_t* upstream_conn   = connect->handle;
+    stream_conn_t* downstream_conn = upstream_conn->data->stream_conn;
+
+    if (status < 0) {
+        fprintf(stderr, "on_upstream_connect error %s\n", uv_strerror(status));
+
+        cleanup_callback_data(upstream_conn);
+
+        //原本应该向下游发送503错误然后关闭连接
+        //现在为了简化情况，直接关闭与下游的连接
+        cleanup_callback_data(downstream_conn);
+        uv_close((uv_handle_t*)downstream_conn, NULL);
+    } else {
+        fprintf(stdout, "connect to upstream success\n");
+    }
+    
+    uv_read_start((uv_stream_t*)downstream_conn->stream, alloc_buffer, 
+            upstream_read);
+
+    downstream_conn->data->ready = 1;
+
+    transfer_stream_data(downstream_conn, upstream_conn, after_upstream_write);
+
+    free(connect);
+}
+
+int transfer_stream_data(uv_stream_t* from, uv_stream_t* to, 
+        uv_write_cb wrcb) {
+
+    uv_write_t* req = (uv_write_t*)malloc(sizeof(uv_write_t));
+    uv_buf_t wrbuf  = uv_buf_init(from->data->buf->buf, from->data->buf->size);
+    from->data->buf->size = 0;
+    uv_write(req, to, &wrbuf, 1, wrcb);
+
+    return 0;
+}
+void upstream_read(uv_stream_t* client, ssize_t nread, 
+        const uv_buf_t* buf) {
+
+    fprintf(stderr, "upstream_read called\n");
+    if (nread < 0) {
+        if (nread != UV_EOF) {
+            fprintf(stderr, "upstream_read error [%s] in process [%u]\n",
+                    uv_err_name(nread), cur_pid);
+        }
+
+        cleanup_callback_data(client->data->stream_conn->stream);
+        uv_close((uv_handle_t*)client->data->stream_conn->stream, NULL);
+
+        cleanup_callback_data(client);
+        uv_close((uv_handle_t*)client, NULL);
+    } else if (nread > 0) {
+        buf->base[nread] = '\0';
+        fprintf(stdout, "%s", buf->base);
+
+        callback_data_t cb_data = (callback_data_t*)client->data;
+
+        //简化设计，将内容先全部读到上游数据缓存中
+        rts_buf_append(cb_data, buf->base, nread);
+
+        if (cb_data->stream_conn->ready) {
+            transfer_stream_data(client, client->data->stream_conn, 
+                    after_downstream_write);
+        }
+    }
+
+    if (buf->base) {
+        free(buf->base);
+    }
+}
+
+void after_downstream_write(uv_write_t* req, int status) {
+    if (status) {
+        fprintf(stderr, "write to downstream error [%s] in process [%u]\n",
+                uv_strerror(status), cur_pid);
+    }
+    fprintf(stderr, "echo_write called\n");
+    free(req);
+}
+
+int cleanup_callback_data(uv_stream_t* stream) {
+    if (stream->data == NULL) {
+        return 0;
+    }
+
+    if (stream->data->buf) {
+        rts_buf_free(stream->data->buf);
+        stream->data->buf = NULL;
+    }
+
+    if (stream->data->stream_conn) {
+        free(stream->data->stream_conn);
+        stream->data->stream_conn = NULL;
+    }
+
+    free(stream->data);
+    stream->data = NULL;
+
+    return 0;
 }
---
+++ demo/proxy_demo/rts_buf.c
@@ -0,0 +1,68 @@
+#ifndef RTS_BUF_C
+#define RTS_BUF_C
+
+#include <stdlib.h>
+
+typedef struct rts_buf_s rts_buf_t;
+struct rts_buf_s {
+    char* buf;
+    size_t size;
+    size_t capacity;
+};
+rts_buf_t* rts_buf_init(size_t capacity = 1) {
+    char* buf = (char*)malloc(capacity);
+    if (buf == NULL) {
+        return NULL;
+    }
+
+    rts_buf_t* rts_buf = (rts_buf_t*)malloc(sizeof(rts_buf_t));
+    rts_buf->buf      = buf;
+    rts_buf->size     = 0;
+    rts_buf->capacity = capacity;
+
+    return rts_buf;
+}
+void rts_buf_free(rts_buf_t* rts_buf) {
+    if (rts_buf != NULL && rts_buf->buf != NULL) {
+        free(rts_buf->buf);
+    }
+    free(rts_buf);
+}
+
+int rts_buf_append(rts_buf_t* rts_buf, 
+        char* content, 
+        size_t content_len) {
+    if (content == NULL) {
+        return -1;
+    }
+
+    if (rts_buf == NULL) {
+        rts_buf = rts_buf_init(content_len + 1);
+        if (rts_buf == NULL) {
+            return -2;
+        }
+    }
+    
+    size_t alloc_len = content_len + 1; //for '\0'
+
+    if (rts_buf->capacity - rts_buf->size < alloc_len) {
+        int new_cap = rts_buf->size + alloc_len - rts_buf->capacity 
+                >= rts_buf->capacity ? rts_buf->size + alloc_len :
+                    rts_buf->capacity * 2;
+        char* new_buf = (char*)malloc(new_cap);
+        if (new_buf == NULL) {
+            return -3;
+        }
+        strncpy(new_buf, rts_buf->buf, rts_buf->size);
+        rts_buf->capacity = new_cap;
+        free(rts_buf->buf);
+        rts_buf->buf = new_buf;
+    }
+
+    strncpy(rts_buf->buf + rts_buf->size, content, content_len);
+    rts_buf->size += content_len;
+
+    return 0;
+}
+
+#endif //RTS_BUF_C
